b63b3a57430753f28272f4e49de712de4ba0ab80
davidzn2 <davidzn2@gmail.com>
2019-04-30 11:22:57 -0500

    Inicio passport

--- config/index.js
+++ config/index.js
@@ -7,6 +7,10 @@ const config = {
     dbPassword:process.env.DB_PASSWORD,
     dbHost:process.env.DB_HOST,
     dbPort: process.env.DB_PORT,
-    dbName:process.env.DB_NAME
+    dbName:process.env.DB_NAME,
+    AUTH_ADMIN_USERNAME:process.env.AUTH_ADMIN_USERNAME,
+    AUTH_ADMIN_PASSWORD:process.env.AUTH_ADMIN_PASSWORD,
+    AUTH_ADMIN_EMAIL:process.env.AUTH_ADMIN_EMAIL,
+    AUTH_JWT_SECRET: process.env.AUTH_JWT_SECRET
 }
 module.exports = {config};
---
+++ services/mongo/seed-admin.js
@@ -0,0 +1,4 @@
+const bcrypt = require('bcrypt');
+const chalk = require('chalk');
+const MongoLib = require('../../lib/mongo');
+const { config } = require('../../config')
